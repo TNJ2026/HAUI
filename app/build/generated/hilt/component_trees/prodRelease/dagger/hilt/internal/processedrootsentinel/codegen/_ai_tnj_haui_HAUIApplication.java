c8510fdeba6d4d9b6f1e6a66b0ed4ab7226e3008
TNJ2026 <chengxd5@gmail.com>
2026-05-12 20:15:52 +0800

    Remove all build folders from remote

--- app/build/generated/hilt/component_trees/prodRelease/dagger/hilt/internal/processedrootsentinel/codegen/_ai_tnj_haui_HAUIApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "ai.tnj.haui.HAUIApplication"
-)
-public final class _ai_tnj_haui_HAUIApplication {
-}
